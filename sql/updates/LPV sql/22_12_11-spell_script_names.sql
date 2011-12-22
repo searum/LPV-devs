efd6e9e7e34450275a182e7e4b2d845582a09f95
searum <se4rum@gmail.com>
2011-12-22 06:03:00 +0100

    ouch, mejor asi ^^

--- sql/updates/LPV sql/22_12_11-spell_script_names.sql
+++ sql/updates/LPV sql/22_12_11-spell_script_names.sql
@@ -1,1 +1,1 @@
-INSERT INTO `world`.`spell_script_names` (`spell_id`, `ScriptName`) VALUES ('31930', 'spell_pal_judgements_of_the_wise');
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES ('31930', 'spell_pal_judgements_of_the_wise');
